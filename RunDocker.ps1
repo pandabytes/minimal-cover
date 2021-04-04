1e8f92f775b95e16e6320e2d5f5fb3c7824fd269
Huy Ngo <huybngo@yahoo.com>
2021-04-04 14:02:55 -0700

    Add tag to docker image

--- RunDocker.ps1
+++ RunDocker.ps1
@@ -1,5 +1,11 @@
+$Tag = "v1.0.0"
+$ImageName = "minimalcover-webapi:$Tag"
+
+# Remove existing image 
+& docker rmi $ImageName
+
 # Build docker image
-docker build -t minimalcover-webapi -f .\src\MinimalCover.UI.WebApi\Dockerfile .
+& docker build -t $ImageName -f .\src\MinimalCover.UI.WebApi\Dockerfile .
 
 # Run docker
-docker run --rm -p 5000:80 -it minimalcover-webapi
+& docker run --rm -p 5000:80 -it $ImageName
