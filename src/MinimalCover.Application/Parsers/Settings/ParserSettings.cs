f73be53d6f9db5cebd76f5cb53c0f0c5cd8ac5c0
Huy Ngo <huybngo@yahoo.com>
2021-04-16 09:12:28 -0700

    Add missing ParserSettings

---
+++ src/MinimalCover.Application/Parsers/Settings/ParserSettings.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MinimalCover.Application.Parsers.Settings
+{
+  /// <summary>
+  /// 
+  /// </summary>
+  public class ParserSettings
+  {
+    public static readonly string SectionPath = "ParsersSettings";
+
+    public TextParserSettings? TextParser { get; init; }
+
+    public JsonParserSettings? JsonParser { get; init; }
+  }
+}
