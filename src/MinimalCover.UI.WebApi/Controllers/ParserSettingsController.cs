b34969f57652684fe8a125932efcdb8de1ccb661
Huy Ngo <huybngo@yahoo.com>
2021-04-16 09:12:53 -0700

    Add ParserSettings controller

---
+++ src/MinimalCover.UI.WebApi/Controllers/ParserSettingsController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+using MinimalCover.Application.Parsers.Settings;
+using MinimalCover.UI.WebApi.Models;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+
+namespace MinimalCover.UI.WebApi.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class ParserSettingsController : ControllerBase
+  {
+    private readonly ParserSettings m_parserSettings;
+
+    /// <summary>
+    /// Constructor
+    /// </summary>
+    /// <param name="settings"></param>
+    public ParserSettingsController(ParserSettings settings) => m_parserSettings = settings;
+
+    [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(ParserSettingsDto))]
+    public IActionResult GetParserSettings()
+    {
+      var settingsDto = new ParserSettingsDto { Settings = m_parserSettings };
+      return Ok(settingsDto);
+    }
+
+  }
+}
---
+++ src/MinimalCover.UI.WebApi/Models/ParserSettingsDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using MinimalCover.Application.Parsers.Settings;
+using System.ComponentModel.DataAnnotations;
+
+namespace MinimalCover.UI.WebApi.Models
+{
+  public class ParserSettingsDto
+  {
+    [Required]
+    public ParserSettings Settings { get; init; } = null!;
+  }
+}
